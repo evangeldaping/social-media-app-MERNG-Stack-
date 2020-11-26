0a3e50684743e147a0e6a5457ff538b3924081c6
evangel <evangeldaping@gmail.com>
2020-11-25 22:43:10 -0800

    displaying comments, adding and deleting comments

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -5,28 +5,30 @@ import { Button, Confirm, Icon } from 'semantic-ui-react';
 
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
-function DeleteButton({ postId, callback }) {
+function DeleteButton({ postId, commentId, callback }) {
   const [confirmOpen, setConfirmOpen] = useState(false);
 
- const [deletePost] = useMutation(DELETE_POST_MUTATION, {
-        refetchQueries: [{ query: FETCH_POSTS_QUERY }],
+  const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
 
-        update(proxy) {
-            setConfirmOpen(false);
+  const [deletePostOrMutation] = useMutation(mutation, {
+    refetchQueries: [{ query: FETCH_POSTS_QUERY }],
 
-            //  const data = proxy.readQuery({
-            //      query: FETCH_POSTS_QUERY,
-            //  });
-            //  data.getPosts = data.getPosts.filter((p) => p.id !== postId);
-            //  proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
-
-            if (callback) callback();
-        },
-
-        variables: {
-            postId,
-        },
-    });
+    update(proxy, result) {
+      setConfirmOpen(false);
+      if (!commentId) {
+        const data = proxy.readQuery({ query: FETCH_POSTS_QUERY });
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                data: { getPosts: [result.data.createPost, ...data.getPosts] }
+            }); 
+      }
+      if (callback) callback();
+    },
+    variables: {
+      postId,
+      commentId
+    }
+  });
   return (
     <>
       <Button
@@ -40,7 +42,7 @@ function DeleteButton({ postId, callback }) {
       <Confirm
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
-        onConfirm={deletePost}
+        onConfirm={deletePostOrMutation}
       />
     </>
   );
@@ -49,6 +51,21 @@ function DeleteButton({ postId, callback }) {
 const DELETE_POST_MUTATION = gql`
   mutation deletePost($postId: ID!) {
     deletePost(postId: $postId)
+  }
+`;
+
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+    }
   }
 `;
 
